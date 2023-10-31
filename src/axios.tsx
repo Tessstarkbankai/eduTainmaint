3c98f09a6bd3692a5e599a2f9adc468d1ad60b4a
vpsop <vpsop544@gmail.com>
2023-10-31 16:41:23 +0530

    chnage base url

--- src/axios.tsx
+++ src/axios.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const client = axios.create({
-	// baseURL: "https://us-central1-eduutainment.cloudfunctions.net/app",
-	baseURL: "http://127.0.0.1:5001/eduutainment/us-central1/app",
+	baseURL: "https://us-central1-eduutainment.cloudfunctions.net/app",
+	// baseURL: "http://127.0.0.1:5001/eduutainment/us-central1/app",
 });
